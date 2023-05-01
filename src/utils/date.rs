7c6059000c1bf48e8d228958eca15578994af998
Gabriel Rech Brand <rcgabrielbrand@gmail.com>
2023-05-01 19:12:18 -0300

    master - Deleting unused code.

--- src/utils/date.rs
+++ src/utils/date.rs
@@ -4,8 +4,3 @@ use chrono::Local;
 pub fn get_current_datetime() -> DateTime<Local> {
     Local::now()
 }
-
-pub fn main() {
-    let current_datetime = get_current_datetime();
-    println!("The current date and time is: {}", current_datetime);
-}
